3f591e384f013cb38965decdb6103af38389ff10
Hayden Shelton <hayden.shelton@utah.edu>
2015-10-25 02:07:04 -0600

    incremental progress

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,22 +1,68 @@
 __author__ = 'Hayden'
-__name__ = "Classifier"
+__name__ = "QuestionType"
 
-import nltk;
+import string
 
 # response categories
-RESPONSE_CATEGORIES = ["NAMED ENTITY", "LIST", "THING", "EXPLANATION"]
+RESPONSE_CATEGORIES = ['NAMED ENTITY',  'THING', 'LIST', 'EXPLANATION']
 
 # this is a dictionary
-RESPONSE_SUBCATEGORIES = {'NAMED ENTITY': ["PERSON", "LOCATION", "TIME"]}
+RESPONSE_SUBCATEGORIES = {'NAMED ENTITY': ['PERSON', 'GROUP', 'PERSONORGROUP' 'LOCATION', 'TIME'],  'THING':['QUANTIFIER', 'QUANTITY'], 'LIST': [], 'EXPLANATION': []}
+
+
+def containsTimeReference(tokens):
+
+    timePhrases = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'hour', 'day', 'week', 'month', 'year']
+    for token in tokens:
+        if token in timePhrases:
+            return True
+
+    return False
+
+
 
 class Classifier:
+
+    responseCategory = "NONE"
+    responseSubcategory = "NONE"
+
+
     def __init__(self, sentence):
-       self.sentence = sentence
+       lower = str.lower(sentence).rstrip('\r\n')
+       tokens = lower.split()
+
+       if tokens.__contains__("who"):
+           self.responseCategory = RESPONSE_CATEGORIES[0]
+           if tokens.__contains__("were") or tokens.__contains__("are"):
+               self.responseSubcategory = 'GROUP'
+           if (tokens.__contains__("was") or tokens.__contains__("is")):
+               self.responseSubcategory = 'PERSON'
+           else:
+               self.responseSubcategory = 'PERSONORGROUP'
 
 
 
+       elif tokens.__contains__("where"):
+           self.responseCategory = RESPONSE_CATEGORIES[0]
+           self.responseSubcategory = 'LOCATION'
 
+       elif (tokens.__contains__("when") or containsTimeReference(tokens) is True):
+           self.responseCategory = RESPONSE_CATEGORIES[0]
+           self.responseSubcategory = 'TIME'
 
+       elif tokens.__contains__("list"):
+            self.responseCategory = RESPONSE_CATEGORIES[2]
+
+       elif (tokens.__contains__("why") or tokens.__contains__("how") or tokens.__contains__("reason") or tokens.__contains__("explain")):
+           self.responseCategory = RESPONSE_CATEGORIES[3]
+           if(tokens.__contains__("how") and tokens.__contains__("much")):
+               self.responseSubcategory = "QUANTIFIER"
+           elif(tokens.__contains__("how") and tokens.__contains__("many")):
+               self.responseSubcategory = "QUANTITY"
+           elif(tokens.__contains__("how") and tokens.__contains__("long")):
+               self.responseSubcategory = "QUANTITY"
+       elif (tokens.__contains__("what")):
+           self.responseCategory = RESPONSE_CATEGORIES[1]
 
 
 
--- src/main.py
+++ src/main.py
@@ -3,10 +3,8 @@ import math
 import string
 import overlap
 import NamedEntityRecognizer
-
+import Classifier
 def main():
-
-
 
     global textList
     textList = []
@@ -14,32 +12,20 @@ def main():
     global questionList
     questionList = []
     ner = NamedEntityRecognizer.NamedEntityRecognizer()
-    textFile = open('text.txt','r')
+    textFile = open('textLonger.txt','r')
     textList = formatFileToList(textFile)
     textFile.close()
 
-    with open('text.txt', 'r') as f:
-        lines = f.readlines()
-        for s in lines:
-            print ("\nLine: " + s)
-            #ner.initialize( str(s).decode(sys.getfilesystemencoding()))
-            ner.initialize(s)
-            ner.printEntities()
-
-
-
-    questionFile = open('questions.txt','r')
+    questionFile = open('longerQuestions.txt','r')
     questionList = formatFileToList(questionFile)
     questionFile.close()
-
-
-
-
-
 
     for q in questionList:
         print ("\nQuestion: " + q)
 
+        classifierResult = Classifier.Classifier(q)
+        print("Response category: " +  classifierResult.responseCategory)
+        print("Response subcategory: " + classifierResult.responseSubcategory)
         print ("Best overlap sentence: " + overlap.bestOverlapCount(q, textList))
         
 def formatFileToList(file):
@@ -47,7 +33,6 @@ def formatFileToList(file):
     for line in file:
         #change to lowercase and remove line breaks
         toLower = str.lower(line).rstrip('\r\n')
-
 
         #remove punctuation
         formattedLine = "".join(c for c in toLower if c not in string.punctuation)
