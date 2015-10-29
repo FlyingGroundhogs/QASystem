2517b929b9225741c12324947ba34dbad2bf4e41
Mike Miner <U0885871@media.utah.edu>
2015-10-29 10:41:12 -0600

    updated main to delimit by correct punctuation

--- src/main.py
+++ src/main.py
@@ -12,28 +12,34 @@ def main():
     questionList = []
 
     textFile = open('text.txt','r')
-    textList = formatFileToList(textFile)
+    textList = formatFileToList(textFile, '.')
     textFile.close()
 
     questionFile = open('questions.txt','r')
-    questionList = formatFileToList(questionFile)
+    questionList = formatFileToList(questionFile, '?')
     questionFile.close()
 
     for q in questionList:
         print ("\nQuestion: " + q)
         print ("Best overlap sentence: " + overlap.bestOverlapCount(q, textList))
         
-def formatFileToList(file):
-    fileList = []
-    for line in file:
-        #change to lowercase and remove line breaks
-        toLower = str.lower(line).rstrip('\r\n')
+def formatFileToList(file, delimiter):
+
+    formattedList = []
+    data = file.read()
+    sentenceList = data.split(delimiter)
+    
+    for line in sentenceList:
+        #change to lowercase, remove line breaks, strip trailing whitespace
+        toLower = str.lower(line).replace('\n','').strip()
         
         #remove punctuation
         formattedLine = "".join(c for c in toLower if c not in string.punctuation)
-        fileList.append(formattedLine)
+        #only add to main list if string is non-empty
+        if formattedLine:
+            formattedList.append(formattedLine)
 
-    return fileList
+    return formattedList
 
 main()
     
